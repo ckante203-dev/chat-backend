4e1989bb102da09ed3a7a3dc6ee54ee84fabe2b0
Kante <ckante203@gmail.com>
2026-01-14 10:52:42 +0100

    Mise à jour db.js pour Supabase avec SSL

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+/*const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
@@ -6,6 +6,22 @@ const pool = new Pool({
   database: 'rivo_db',
   password: 'DIRECT@2017',
   port: 5432,
+});
+
+module.exports = pool;
+*/
+
+require('dotenv').config();
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false } // obligatoire pour Supabase
 });
 
 module.exports = pool;
